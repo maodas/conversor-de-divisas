da08249b882b546108ff52b5c056b74ab9a37943
maodas <rodas807@gmail.com>
2023-11-20 23:03:37 -0600

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+  templateUrl: './app.component.html',
+  
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+
+  cantidadEnQuetzales!: number;
+  cantidadEnDolares : number = 0;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  convertir() {
+    this.cantidadEnDolares = this.cantidadEnQuetzales / 8;
+  }
+
 }
